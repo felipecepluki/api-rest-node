f2a0af896c7012458b53b29dd5d5e2dde693c243
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-15 12:26:46 -0300

    Finish the project

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,29 +2,35 @@ import livros from "../models/Livro.js";
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.status(200).json(livros);
-    });
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.status(200).json(livros);
+      });
   };
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
-    livros.findById(id, (err, livros) => {
-      if (err) {
-        res
-          .status(400)
-          .send({ message: `${err.message} - Id do livro não localizado` });
-      } else {
-        res.status(200).send(livros);
-      }
-    });
+    livros
+      .findById(id)
+      .populate("autor", "nome")
+      .exec((err, livros) => {
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - Id do livro não localizado.` });
+        } else {
+          res.status(200).send(livros);
+        }
+      });
   };
   static cadastrarLivro = (req, res) => {
     let livro = new livros(req.body);
     livro.save((err) => {
       if (err) {
-        res.status(500).send({
-          message: `${err.message} - Falha ao cadastrar livro`,
-        });
+        res
+          .status(500)
+          .send({ message: `${err.message} - falha ao cadastrar livro.` });
       } else {
         res.status(201).send(livro.toJSON());
       }
@@ -44,14 +50,16 @@ class LivroController {
     const id = req.params.id;
     livros.findByIdAndDelete(id, (err) => {
       if (!err) {
-        res.status(200).send({
-          message: "Livro removido com sucesso",
-        });
+        res.status(200).send({ message: "Livro removido com sucesso" });
       } else {
-        res.status(500).send({
-          message: err.message,
-        });
+        res.status(500).send({ message: err.message });
       }
+    });
+  };
+  static listarLivroPorEditora = (req, res) => {
+    const editora = req.query.editora;
+    livros.find({ editora: editora }, {}, (err, livros) => {
+      res.status(200).send(livros);
     });
   };
 }
